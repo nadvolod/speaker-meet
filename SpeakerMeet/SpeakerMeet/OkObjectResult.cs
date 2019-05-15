f077275b7262e76c13c309a4395fbabb4f8db437
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-05-15 18:29:31 -0400

    Refactor and add another test

--- SpeakerMeet/SpeakerMeet/OkObjectResult.cs
+++ SpeakerMeet/SpeakerMeet/OkObjectResult.cs
@@ -1,6 +1,9 @@
-﻿namespace SpeakerMeet
+﻿using System.Collections.Generic;
+
+namespace SpeakerMeet
 {
     internal class OkObjectResult
     {
+        public List<Speaker> Value => new List<Speaker>();
     }
 }
--- SpeakerMeet/SpeakerMeet/SpeakerControllerSearchTests.cs
+++ SpeakerMeet/SpeakerMeet/SpeakerControllerSearchTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -6,13 +7,27 @@ namespace SpeakerMeet
     [TestClass]
     public class SpeakerControllerSearchTests
     {
+        private SpeakerController controller;
+        [TestInitialize]
+        public void Setup()
+        {
+            controller = new SpeakerController();
+        }
         [TestMethod]
         public void ItHasSearch()
         {
-            var controller = new SpeakerController();
             var result = controller.Search("Jos");
             result.Should().NotBeNull();
             result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [TestMethod]
+        public void ItReturnsCollectionOfSpeakers()
+        {
+            var result = controller.Search("Jos") as OkObjectResult;
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+            result.Value.Should().BeOfType(typeof(List<Speaker>));
         }
     }
 }
